3e14d923b6f9bf2e1d5f0e9edbbf834d9891ab96
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-01-24 23:34:18 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import {ServiceService} from './service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+   constructor(private _auth: ServiceService) {}
+   userModel = new User('Enter your Data');
+   onSubmit() {
+      this._auth.postFunction(this.userModel).subscribe(res => console.log(res));
+  }
 }
