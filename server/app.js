fa0c933ff1d2abf1b7dc3f61e417d831a422faf8
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-01-26 22:20:20 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,23 +9,38 @@ app.use(parser.json());
 
 const mongodbURL = 'mongodb://himanshuchauhan:himanshu0018@ds157834.mlab.com:57834/todoapp';
 mongoose.connect(mongodbURL,{ useNewUrlParser: true });
+
+//Creating Schema for Database
 let todoSchema = mongoose.Schema({
     userData: String
 });
 datamodel = mongoose.model('todoWork',todoSchema);
 
+//Saving data to database
 app.post('/api/storingData',function(request,response){
     data = {userData: request.body.userData};
     var model = new datamodel(data);
     model.save();
 });
 
+//Getting data from database
 app.get('/api/showData',function(request,response){
     datamodel.find({},function(err,data){
-        return response.json({data});
+      return response.json({data});
     });
 });
 
+//Deleting Data from Database
+app.post('/api/deleteData/:data',function(request,response){
+   datamodel.remove({userData:request.params.data},function(err,docs){
+      if(docs){
+         console.log(docs);
+      }
+   });
+   response.status(200);
+});
+
+//Creating Servers
 app.listen(1234,function(){
    console.log('Server is running at 1234');
 });
